0afcc6d961eaeb2b8d201b09a26498df388c0fd3
JSU <nkey@stu.jsu.edu>
2022-02-01 13:57:55 -0600

    added field for output

---
+++ app/src/main/java/Weapon.java
@@ -0,0 +1,13 @@
+public enum Weapon {
+
+    ROCK("Rock"),
+    PAPER("Paper"),
+    SCISSORS("Scissors");
+    private String message;
+
+    private Weapon(String msg) { message = msg; }
+
+    @Override
+    public String toString() { return message; }
+
+};
